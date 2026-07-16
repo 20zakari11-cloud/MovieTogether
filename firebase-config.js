2ee5e2e27646f89becf26608c063c92d53487750
20zakari11-cloud <20zakari11@gmail.com>
2026-07-16 19:49:27 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+/* =========================================================================
+   firebase-config.js
+   -----------------------------------------------------------------------
+   إعدادات مشروع Movie Together على فايربيس.
+   ========================================================================= */
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.MOVIE_TOGETHER_FIREBASE_CONFIG = {
   apiKey: "AIzaSyDZQg2jNxdbaKWLxHAxh4rzz-IUp35BbBk",
   authDomain: "movietogether-88b89.firebaseapp.com",
   databaseURL: "https://movietogether-88b89-default-rtdb.firebaseio.com",
   projectId: "movietogether-88b89",
   storageBucket: "movietogether-88b89.firebasestorage.app",
   messagingSenderId: "996110514025",
-  appId: "1:996110514025:web:24184ce9992a3a5ffaaff8",
-  measurementId: "G-R6GYV9Z1JZ"
+  appId: "1:996110514025:web:24184ce9992a3a5ffaaff8"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
